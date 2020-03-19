3f7b761ae54266c4ef8b7bba00a32ec90c93c4ea
Matthias Egli <matthias.egli@stud.hslu.ch>
2020-03-19 10:58:40 +0100

    Comply with naming convention and formatting. Dependency added.

--- sonar_sensorinput/read_sonar.py
+++ sonar_sensorinput/read_sonar.py
@@ -1,70 +1,71 @@
-#Libraries
+# Libraries
 import Jetson.GPIO as GPIO
 import time
- 
-#GPIO Mode sets pin addressing scheme
+
+# GPIO Mode sets pin addressing scheme
 GPIO.setmode(GPIO.BOARD)
- 
-#set GPIO Pins
+
+# set GPIO Pins
 GPIO_TRIGGER_A = 12
 GPIO_ECHO_A = 16
 GPIO_TRIGGER_B = 18
 GPIO_ECHO_B = 22
- 
-#set GPIO direction
+
+# set GPIO direction
 GPIO.setup(GPIO_TRIGGER_A, GPIO.OUT)
 GPIO.setup(GPIO_ECHO_A, GPIO.IN)
 GPIO.setup(GPIO_TRIGGER_B, GPIO.OUT)
 GPIO.setup(GPIO_ECHO_B, GPIO.IN)
+
 
 class ReadSonar:
     @staticmethod
     def getDistance():
         # set TriggerA to HIGH
         GPIO.output(GPIO_TRIGGER_A, True)
-    
+
         # set TriggerA after 0.01ms to LOW
         time.sleep(0.00001)
         GPIO.output(GPIO_TRIGGER_A, False)
-    
-        StartTime = time.time()
-        StopTime = time.time()
-    
-        # save StartTime
+
+        start_time = time.time()
+        stop_time = time.time()
+
+        # save start_time
         while GPIO.input(GPIO_ECHO_A) == 0:
-            StartTime = time.time()
-    
+            start_time = time.time()
+
         # save time of arrival
         while GPIO.input(GPIO_ECHO_A) == 1:
-            StopTime = time.time()
-    
+            stop_time = time.time()
+
         # time difference between start and arrival
-        TimeElapsed = StopTime - StartTime
+        time_elapsed = stop_time - start_time
         # multiply with the sonic speed (34300 cm/s)
         # and divide by 2, because there and back
-        distance_A = (TimeElapsed * 34300) / 2
-    
+        distance_a = (time_elapsed * 34300) / 2
+
         # set TriggerB to HIGH
         GPIO.output(GPIO_TRIGGER_B, True)
-    
+
         # set TriggerB after 0.01ms to LOW
         time.sleep(0.00001)
         GPIO.output(GPIO_TRIGGER_B, False)
-    
-        # save StartTime
+
+        # save start_time
         while GPIO.input(GPIO_ECHO_B) == 0:
-            StartTime = time.time()
-    
+            start_time = time.time()
+
         # save time of arrival
         while GPIO.input(GPIO_ECHO_B) == 1:
-            StopTime = time.time()
-    
+            stop_time = time.time()
+
         # time difference between start and arrival
-        TimeElapsed = StopTime - StartTime
+        time_elapsed = stop_time - start_time
         # multiply with the sonic speed (34300 cm/s)
         # and divide by 2, because there and back
-        distance_B = (TimeElapsed * 34300) / 2
-    
+        distance_b = (time_elapsed * 34300) / 2
+
         GPIO.cleanup()
-        
-        return distance_A, distance_B
+
+        return distance_a, distance_b
