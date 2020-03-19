7e1fc3123e79ee897582910ae424c878d9361c87
Matthias Egli <matthias.egli@stud.hslu.ch>
2020-03-19 11:06:14 +0100

    Renamed element and applied converter method correctly

--- statemachine/radius_to_steering_angel_converter.py
+++ statemachine/radius_to_steering_angel_converter.py
@@ -6,6 +6,6 @@ class RadiusToSteeringAngelConverter:
         self._wheel_distance = 250  # millimetre
         self._distance_center_of_gravity_read_wheels = 150  # millimetre
 
-    def convert(self, curve_radius: float) -> float:
+    def convert(self, curve_radius_centimeters: float) -> float:
         return np.arctan(
-            self._wheel_distance / np.sqrt((curve_radius ** 2) - (self._distance_center_of_gravity_read_wheels ** 2)))
+            self._wheel_distance / np.sqrt((curve_radius_centimeters ** 2) - (self._distance_center_of_gravity_read_wheels ** 2)))
