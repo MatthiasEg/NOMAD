5c885fe9f0118fcc9617588b080edc9bc359304a
Matthias Egli <matthias.egli@stud.hslu.ch>
2020-03-19 10:34:18 +0100

    getter added to ObjectDetector Result and all its dependencies

--- object_detection/object_detector/object_detector_result.py
+++ object_detection/object_detector/object_detector_result.py
@@ -13,8 +13,15 @@ class DetectedObjectType(Enum):
 class Distance:
     def __init__(self, value: float, measured: bool):
         self._value = value
-        # true means measured by distance sensors, false means estimated by camera
-        self._measured = measured
+        self._measured = measured  # true means measured by distance sensors, false means estimated by camera
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+    @property
+    def measured(self) -> bool:
+        return self._measured
 
     def __str__(self):
         return "Distance: [value='%s', measured='%s']" % (self._value, self._measured)
@@ -29,17 +36,27 @@ class RelativeObjectType(Enum):
 
 class RelativeObject:
     def __init__(self, detected_object: DetectedObject, relative_type: RelativeObjectType):
-        self.detected_object = detected_object
-        self.relative_type = relative_type
+        self._detected_object = detected_object
+        self._relative_type = relative_type
+
+    @property
+    def detected_object(self) -> DetectedObject:
+        return self._detected_object
+
+    @property
+    def relative_type(self) -> RelativeObjectType:
+        return self._relative_type
 
     def __str__(self):
-        return "RelativeObject: [detected_object='%s', relative_type='%s']" % (self.detected_object, self.relative_type)
+        return "RelativeObject: [detected_object='%s', relative_type='%s']" % (
+            self._detected_object, self._relative_type)
 
 
 class DetectedObject:
     """
     Obstacles or Pylon
     """
+
     def __init__(self, object_type: DetectedObjectType, bounding_box: BoundingBox, distance: Distance,
                  relative_objects: List[RelativeObject]):
         self._object_type = object_type
@@ -47,19 +64,44 @@ class DetectedObject:
         self._distance = distance  # nullable
         self._relative_objects = relative_objects
 
-    def __str__(self):
-        relative_objects_string_representation = ""
-        for relative_object in self._relative_objects:
-            relative_objects_string_representation += str(relative_object)
-        return "DetectedObject: [type='%s', boundingBox='%s', distance='%s', " \
-               "relative_objects_string_representation='%s']" % \
-               (self._object_type, self._bounding_box, self._distance, relative_objects_string_representation)
+    @property
+    def object_type(self) -> DetectedObjectType:
+        return self._object_type
+
+    @property
+    def bounding_box(self) -> BoundingBox:
+        return self._bounding_box
+
+    @property
+    def distance(self) -> Distance:
+        return self._distance
+
+    @property
+    def relative_objects(self) -> List[RelativeObject]:
+        return self._relative_objects
+
+
+def __str__(self):
+    relative_objects_string_representation = ""
+    for relative_object in self._relative_objects:
+        relative_objects_string_representation += str(relative_object)
+    return "DetectedObject: [type='%s', boundingBox='%s', distance='%s', " \
+           "relative_objects_string_representation='%s']" % \
+           (self._object_type, self._bounding_box, self._distance, relative_objects_string_representation)
 
 
 class ObjectDetectorResult:
     def __init__(self, detected_objects: List[DetectedObject]):
         self._timestamp = datetime.timestamp(datetime.now())
         self._detected_objects = detected_objects
+
+    @property
+    def detected_objects(self) -> List[DetectedObject]:
+        return self._detected_objects
+
+    @property
+    def timestamp(self) -> float:
+        return self._timestamp
 
     def __str__(self):
         detected_objects_string_representation = ""
