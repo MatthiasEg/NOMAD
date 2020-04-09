7733ab2fab74242dd130660b2cbde9ec95e5566c
Matthias Egli <matthias.egli@stud.hslu.ch>
2020-04-09 13:30:32 +0200

    moved and renamed imu and sonar classes

---
+++ sensors/imu.py
@@ -0,0 +1,72 @@
+# Libraries
+from smbus2 import SMBus  # import SMBus2 module since original smbus module is incompatible with python3.8
+from time import sleep
+
+# set MPU6050 registers and their address
+Device_Address = 0x68  # MPU6050 device address
+PWR_MGMT_1 = 0x6B
+SMPLRT_DIV = 0x19
+CONFIG = 0x1A
+GYRO_CONFIG = 0x1B
+INT_ENABLE = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H = 0x43
+GYRO_YOUT_H = 0x45
+GYRO_ZOUT_H = 0x47
+
+
+class IMU:
+    # create class instance as singleton to prevent multiple I2C initialization attempts
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+    def __init__(self):
+        self.bus = SMBus(1)  # select I2C bus 1 (pin 3 & 5)
+        self.MPU_Init()
+
+    def MPU_Init(self):
+        # write to sample rate register
+        self.bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+
+        # write to power management register
+        self.bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+
+        # write to Configuration register
+        self.bus.write_byte_data(Device_Address, CONFIG, 0)
+
+        # write to Gyro configuration register
+        self.bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+
+        # write to interrupt enable register
+        self.bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+    def read_raw_data(self, addr):
+        # accelerometer and gyroscope values are 16-bit
+        high = self.bus.read_byte_data(Device_Address, addr)
+        low = self.bus.read_byte_data(Device_Address, addr + 1)
+
+        # concatenate higher and lower value
+        value = ((high << 8) | low)
+
+        # to get signed value from mpu6050
+        if value > 32768:
+            value = value - 65536
+        return value
+
+    # read accelerometer value
+    def get_accel(self):
+        acc_x = self.read_raw_data(ACCEL_XOUT_H) / 16384.0
+        acc_y = self.read_raw_data(ACCEL_YOUT_H) / 16384.0
+        acc_z = self.read_raw_data(ACCEL_ZOUT_H) / 16384.0
+        return acc_x, acc_y, acc_z
+
+    # read gyroscope value
+    def get_gyro(self):
+        gyro_x = self.read_raw_data(GYRO_XOUT_H) / 131.0
+        gyro_y = self.read_raw_data(GYRO_YOUT_H) / 131.0
+        gyro_z = self.read_raw_data(GYRO_ZOUT_H) / 131.0
+        return gyro_x, gyro_y, gyro_z
---
+++ sensors/sonar.py
@@ -0,0 +1,69 @@
+# Libraries
+import Jetson.GPIO as GPIO
+import time
+
+# GPIO Mode sets pin addressing scheme
+GPIO.setmode(GPIO.BOARD)
+
+# set GPIO Pins
+GPIO_TRIGGER_A = 12
+GPIO_ECHO_A = 16
+GPIO_TRIGGER_B = 18
+GPIO_ECHO_B = 22
+
+# set GPIO direction
+GPIO.setup(GPIO_TRIGGER_A, GPIO.OUT)
+GPIO.setup(GPIO_ECHO_A, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER_B, GPIO.OUT)
+GPIO.setup(GPIO_ECHO_B, GPIO.IN)
+
+
+class Sonar:
+    @staticmethod
+    def getDistance():
+        # set TriggerA to HIGH
+        GPIO.output(GPIO_TRIGGER_A, True)
+
+        # set TriggerA after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER_A, False)
+
+        start_time = time.time()
+        stop_time = time.time()
+
+        # save start_time
+        while GPIO.input(GPIO_ECHO_A) == 0:
+            start_time = time.time()
+
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO_A) == 1:
+            stop_time = time.time()
+
+        # time difference between start and arrival
+        time_elapsed = stop_time - start_time
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance_a = (time_elapsed * 34300) / 2
+
+        # set TriggerB to HIGH
+        GPIO.output(GPIO_TRIGGER_B, True)
+
+        # set TriggerB after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER_B, False)
+
+        # save start_time
+        while GPIO.input(GPIO_ECHO_B) == 0:
+            start_time = time.time()
+
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO_B) == 1:
+            stop_time = time.time()
+
+        # time difference between start and arrival
+        time_elapsed = stop_time - start_time
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance_b = (time_elapsed * 34300) / 2
+
+        return distance_a, distance_b
