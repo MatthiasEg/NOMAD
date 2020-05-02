2931c481c0957be4caff9f0197fea40ae397cea0
von Uslar Cyrill tavonusl <cyrill.vonuslar@hslu.ch>
2020-05-02 19:24:21 +0200

    add class to write steering commands to file

--- uart_connector/uart_connector.py
+++ uart_connector/uart_connector.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import serial
+import re
 
 from communication.node import Node
 from communication.receiver import Receiver
@@ -35,3 +36,26 @@ class UartConnector(Node):
     def _shut_down(self):
         self._steering_command_generator_receiver.close()
         self.ser.close()
+
+class FileConnector(Node):
+    _node_config_name = "FILE_CONNECTOR"
+    _logger = logging.getLogger("FileConnector")
+
+    def __init__(self):
+        super().__init__(self._node_config_name)
+
+    def _start_up(self):
+        self._steering_command_generator_receiver = Receiver("STEERING_COMMAND_GENERATOR")
+        self.file = open('steeringcommands.txt', "w")
+
+    def _progress(self):
+        self._steering_command_generator_result = self._steering_command_generator_receiver.receive()
+        if not self.file.closed:
+            angle = re.search(r"(?<=(steering_angel=))(.*?)(?=\,)", self._steering_command_generator_result).group(0)
+            speed = re.search(r"(?<=(velocity=))(.*?)(?=\,)", self._steering_command_generator_result).group(0)
+            self.file.write(f"{angle};{speed}\n")
+            self._logger.debug(f"wrote {angle};{speed} to file")
+
+    def _shut_down(self):
+        self._steering_command_generator_receiver.close()
+        self.file.close()
